6d88e5267554534a5ebad023fdf6cf95b8fd8ad5
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-07-15 12:37:42 +0900

    200715

--- employee/app.js
+++ employee/app.js
@@ -17,18 +17,54 @@ request.get({url:baseUrl, json:true}, function(error, response, body){
         let age = data.employee_age
         let salary = data.employee_salary
 
-        
+        let query = `insert into employee values (${id}, "${name}", ${age}, ${salary})`
 
-        for(let q = 0; q < id.length; q++){
-            let query = 'insert into employee values (?,?,?,?)'
-            let values = [id , name , age, salary]
-    
-            connection.query(query, values,
-                function(error, results, fields){
-                    console.log(results)
-                })
-        }
+        connection.query(query,
+            function(error, results, fields){
+                console.log(results)
+            })
     }
 
+    connection.end()
 })
-connection.end()
+
+
+// const request = require('postman-request')
+
+// const baseUrl = 'http://dummy.restapiexample.com/api/v1/employees'
+
+// const connection = require('./mysql-connection.js')
+
+// request.get({url:baseUrl, json:true}, function(error, response, body){
+//     // console.log(body.data.employee_name)
+
+//     let dataArray = body.data
+//     for(let i = 0; i < dataArray.length; i++){
+        
+//         let data = dataArray[i]
+        
+//         let id = data.id
+//         let name = data.employee_name
+//         let age = data.employee_age
+//         let salary = data.employee_salary
+
+//         // let query = 'insert into employee (employee_name) values '
+//         // let values = '('+id+','+name+','+age+','+salary+')'
+
+//         // console.log ('insert into employee (employee_name) values ' + '('+id+','+name+','+age+','+salary+')')
+
+//         connection.query('insert into employee (employee_name) values ' + '('+id+',"'+name+'",'+age+','+salary+')',
+//             function(error, results, fields){
+//                 console.log(results)
+//             })
+
+//     }
+
+// })
+
+// connection.end()
+
+// // let id = data.id
+// // let name = data.employee_name
+// // let age = data.employee_age
+// // let salary = data.employee_salary
